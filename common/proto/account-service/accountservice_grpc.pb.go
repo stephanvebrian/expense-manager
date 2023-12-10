aa5cd404a8633fc33a591a1496a8313f56fc1aae
stephanvebrian <stephanvebrian@gmail.com>
2023-12-10 18:35:29 +0700

    feat(common): set unimplemented false

--- common/proto/account-service/accountservice_grpc.pb.go
+++ common/proto/account-service/accountservice_grpc.pb.go
@@ -47,21 +47,19 @@ func (c *accountServiceClient) Ping(ctx context.Context, in *GetPingRequest, opt
 }
 
 // AccountServiceServer is the server API for AccountService service.
-// All implementations must embed UnimplementedAccountServiceServer
+// All implementations should embed UnimplementedAccountServiceServer
 // for forward compatibility
 type AccountServiceServer interface {
 	Ping(context.Context, *GetPingRequest) (*GetPingResponse, error)
-	mustEmbedUnimplementedAccountServiceServer()
 }
 
-// UnimplementedAccountServiceServer must be embedded to have forward compatible implementations.
+// UnimplementedAccountServiceServer should be embedded to have forward compatible implementations.
 type UnimplementedAccountServiceServer struct {
 }
 
 func (UnimplementedAccountServiceServer) Ping(context.Context, *GetPingRequest) (*GetPingResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Ping not implemented")
 }
-func (UnimplementedAccountServiceServer) mustEmbedUnimplementedAccountServiceServer() {}
 
 // UnsafeAccountServiceServer may be embedded to opt out of forward compatibility for this service.
 // Use of this interface is not recommended, as added methods to AccountServiceServer will
